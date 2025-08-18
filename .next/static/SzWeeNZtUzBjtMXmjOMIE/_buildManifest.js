6f1cbbb72c0e9e6a3b2582d7d78ac2423d27f447
Yevucee <samuel.polley1@gmail.com>
2025-08-18 18:31:07 +0000

    Clean deploy: source-only, include framer-motion dep

---
+++ .next/static/SzWeeNZtUzBjtMXmjOMIE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/482-60e4d11a902cc46d.js","static/chunks/pages/index-5cc5a884b2bfb471.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
